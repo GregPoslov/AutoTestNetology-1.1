8dc2e319a2f49dca39f7c5e7e79149abeeb58afb
Greg Poslov <grig.skamsk@mail.ru>
2023-02-04 20:47:14 +0500

    TestNG

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,18 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemain() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+        int expected = 100;
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals(actual,expected);
+    }
+}
